920d1f2f661e609f642b682509ba45c76e2ebfb4
Esraa102 <manalellban9@gmailcom>
2024-05-09 07:39:05 +0300

    styling not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="text-center page">
+      <p className="text-8xl font-bold text-pink">404</p>
+      <p>This Page Not Found</p>
+      <Link
+        to={"/"}
+        className="text-pink/80 font-bold hover:underline hover:text-pink transition"
+      >
+        Go Back Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
