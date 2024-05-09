7445a00fc59050cc3d637ae58f69423959b719cd
Esraa102 <manalellban9@gmailcom>
2024-05-09 07:39:15 +0300

    ready for deploying

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,16 @@ import { authRouter } from "./routes/auth.route.js";
 import { messageRouter } from "./routes/message.route.js";
 import { conversationRouter } from "./routes/conversation.route.js";
 import { userRouter } from "./routes/user.route.js";
-
+import path from "path";
 const port = process.env.PORT || 5001;
 connectToDB();
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.JWT_SECRET_TOKEN));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -29,7 +32,9 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/messages", messageRouter);
 app.use("/api/v1/coversations", conversationRouter);
 app.use("/api/v1/users", userRouter);
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(port, () => {
   console.log("Server Is Running on", port);
 });
